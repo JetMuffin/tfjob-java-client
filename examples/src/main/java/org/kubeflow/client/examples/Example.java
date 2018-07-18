904f5475013ebfefcae3353f3e7e20a9bd4a77bd
JetMuffin <mofeng.cj@alibaba-inc.com>
2018-07-18 19:29:53 +0800

    update submit example.
    Signed-off-by: JetMuffin <mofeng.cj@alibaba-inc.com>

--- examples/src/main/java/org/kubeflow/client/examples/Example.java
+++ examples/src/main/java/org/kubeflow/client/examples/Example.java
@@ -17,14 +17,26 @@ public class Example {
               .replicas(1)
               .cpu(1.0)
               .memory(1024.0)
-              .image("registry.cn-hangzhou.aliyuncs.com/jetmuffin/word2vec_kubeflow");
+              .image(
+                  "reg.docker.alibaba-inc.com/pai-tensorflow/tensorflow-build:1.4.0-PAI1805u1-D1530175766-cpu")
+              .env("HADOOP_HDFS_HOME", "/usr/local/hadoop")
+              .env("RESOURCE_PATH", "hdfs://100.81.153.173:8020/tmp/a.tar.gz")
+              .env("ENTRY_FILE", "mnist_hdfs.py")
+              .args(
+                  "--train_tfrecords=hdfs://100.81.153.173:8020/tmp/train.tfrecords --test_tfrecords=hdfs://100.81.153.173:8020/tmp/test.tfrecords");
       TFReplica worker =
           new TFReplica()
               .replicas(1)
               .cpu(2.0)
               .memory(2048.0)
-              .image("registry.cn-hangzhou.aliyuncs.com/jetmuffin/word2vec_kubeflow");
-      Job job = new Job().name("test").ps(ps).worker(worker).cleanupPolicy("running");
+              .image(
+                  "reg.docker.alibaba-inc.com/pai-tensorflow/tensorflow-build:1.4.0-PAI1805u1-D1530175766-cpu")
+              .env("HADOOP_HDFS_HOME", "/usr/local/hadoop")
+              .env("RESOURCE_PATH", "hdfs://100.81.153.173:8020/tmp/a.tar.gz")
+              .env("ENTRY_FILE", "mnist_hdfs.py")
+              .args(
+                  "--train_tfrecords=hdfs://100.81.153.173:8020/tmp/train.tfrecords --test_tfrecords=hdfs://100.81.153.173:8020/tmp/test.tfrecords");
+      Job job = new Job().name("sdk-job").ps(ps).worker(worker).cleanupPolicy("running");
       client.submitJob(job);
 
       List<Job> jobs = client.listJobs();
